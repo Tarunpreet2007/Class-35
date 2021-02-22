f4aee19471964b5990bc3fd7bf3d9946946264bf
Tarunpreet2007 <73647751+Tarunpreet2007@users.noreply.github.com>
2021-02-22 15:58:13 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,32 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title=createElement('h2')
+        title.html("Car Racing Game")
+        title.position(450,0);
+        
+        var input=createInput("name")
+        input.position(450,160);
+        var button=createButton("play")
+        button.position(550,200)
+         
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name=input.value();
+            playerCount=playerCount+1;
+            player.update(name)
+            player.updateCount(playerCount)
+
+            var greeting=createElement('h3')
+            greeting.html("Hello"+name)
+            greeting.position(450,160);
+
+        });
+
+
+    }
+}
+
